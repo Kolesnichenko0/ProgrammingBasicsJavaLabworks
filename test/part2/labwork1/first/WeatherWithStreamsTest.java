6a59f016e3f4aa4a4a37904abe4338cb8260be3e
Kolesnichenko0 <den.koleskos@gmail.com>
2024-04-04 23:16:43 +0300

    Added WeatherWithStreamsTest class

---
+++ test/part2/labwork1/first/WeatherWithStreamsTest.java
@@ -0,0 +1,46 @@
+package part2.labwork1.first;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeatherWithStreamsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void setDays() {
+    }
+
+    @Test
+    void testSetDays() {
+    }
+
+    @Test
+    void sortByDecreasingTemperature() {
+    }
+
+    @Test
+    void sortByComment() {
+    }
+
+    @Test
+    void calculateAverageTemperature() {
+    }
+
+    @Test
+    void findMaxTemperatureDays() {
+    }
+
+    @Test
+    void findLongestCommentDays() {
+    }
+}
